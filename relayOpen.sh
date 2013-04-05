e362ac2f1bce4ea1f6f12413c8e57ed25f2e8bcf
Jacob Melton <jmelton116@gmail.com>
2013-04-05 17:02:38 -0300

    Update relayOpen.sh
    Added note about group and permissions needed to access USB device.

--- relayOpen.sh
+++ relayOpen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # sends on signal to the USB relay
+# You will need to add your self to the dialout group 
+# and likely also nee to execute 'chmod a+rw /dev/ttyUSB0'
+# to allow reading and writing to the device
 echo -e "\xFF\x01\x01" > /dev/ttyUSB0
 echo "OPENED"
