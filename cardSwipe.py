32f8cc6c0a4974e96023686a7f69ba330cb63ec4
Jacob Melton <jmelton116@gmail.com>
2013-04-05 16:59:02 -0300

    Update cardSwipe.py
    Made paths to relay scripts not absolute and fixed comments...sorta.

--- cardSwipe.py
+++ cardSwipe.py
@@ -4,27 +4,27 @@ import subprocess
 print("Program Starting")
 
 accessFile = open("accessList.txt", "r")    	            # opens the access list
-accessList = accessFile.readlines()			                	# creates a list of strings with one ID number per string
-accessFile.close()							                         	# close the file
+accessList = accessFile.readlines()			    # creates a list of strings with one ID number per string
+accessFile.close()					    # close the file
 
-accessList = [number.strip() for number in accessList]		# strips trailing new line characters from the list of strings.
+accessList = [number.strip() for number in accessList]	    # strips trailing new line characters from the list of strings.
 
-while True:									                              # python do while, gets card input, if it isnt right length then repeats
-	cardNum = input('>>')					                          # breaks on proper length string
+while True:					  	    # python do while, gets card input, if it isnt right length then repeats
+	cardNum = input('>>')				    # breaks on proper length string
 	if len(cardNum) == 9:
 		break
 
-while cardNum != "quit":						                      # asks for input reaptedly
-	if cardNum in accessList:				                      	# checks if input number is in accessList
-		subprocess.call("/home/jacob/Documents/ENGE1104/Group Project/relayON.sh")	 # turns the relay on
-		time.sleep(6)																                                 # waits a few seconds to let them open the door
-		subprocess.call("/home/jacob/Documents/ENGE1104/Group Project/relayOFF.sh")  # turns the relay off
+while cardNum != "quit":				    # asks for input reaptedly
+	if cardNum in accessList:			    # checks if input number is in accessList
+		subprocess.call("./relayON.sh")		    # turns the relay on
+		time.sleep(6)				    # waits a few seconds to let them open the door
+		subprocess.call("./relayOFF.sh")  	    # turns the relay off
 	else:
 		print("ID not in Access List - Access Denied!")
 
-	while True:                                             # do repeatedly till we get good input
+	while True:                                         # do repeatedly till we get good input
 		cardNum = input('>>')
-		if len(cardNum) == 9 or cardNum == "quit":            # check we are getting the right length ID or are quiting
+		if len(cardNum) == 9 or cardNum == "quit":  # check we are getting the right length ID or are quiting
 			break
 
 print("You have quit")
